53ecdf549f2bb932092a2a6caba84153f83bbcfa
Daniel Pope <mauve@amethyst.(none)>
2013-07-05 17:15:12 +0100

    Added terrain generation

--- game.py
+++ game.py
@@ -1,11 +1,35 @@
 import pygame
+import random
 
 FPS = 30
 W = 800
 H = 600
-
+GROUND = (0, 128, 0)
 
 terrain = pygame.Surface((W, H))
+
+
+def gen_terrain(terrain):
+    h = 0
+    vh = 0
+    heights = []
+    for x in range(W):
+        vh += random.random() - 0.5
+        h = h + vh + 0.5
+        heights.append(h)
+        vh *= 0.97
+
+    min_h = min(heights)
+    max_h = max(heights)
+
+    hrange = float(max_h - min_h)
+
+    bottom = int(H * 0.2)
+    screen_range = int(H * 0.6)
+    for x, h in enumerate(heights):
+        frac = (h - min_h) / hrange
+        screen_h = int(bottom + screen_range * frac + 0.5)
+        pygame.draw.line(terrain, GROUND, (x, H), (x, H - screen_h))
 
 
 def update(dt):
@@ -14,11 +38,13 @@ def update(dt):
 
 def draw():
     screen.fill((50, 100, 255))
+    screen.blit(terrain, (0, 0))
 
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((W, H))
+    gen_terrain(terrain)
 
     clock = pygame.time.Clock()
     while True:
