5e9d1a3254739a8cd29d2215430dc9fdb4771450
Sven Marnach <sven@marnach.net>
2013-07-05 17:17:17 +0100

    Add logic to move the shots.

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ FPS = 30
 W = 800
 H = 600
 
+g = 9.81
+
 
 terrain = pygame.Surface((W, H))
 
@@ -13,9 +15,20 @@ shot_y = 0.0
 shot_v_x = 0.0
 shot_v_y = 0.0
 
+# angle and start velocity for each player
+angles = [0.0, 0.0]
+start_vs = [0.0, 0.0]
+
 
 def update(dt):
-    
+    if shot_in_flight:
+        shot_x += dt * shot_v_x
+        shot_y += dt * shot_v_y
+        shot_v_y -= g * dt
+        colour = terrain.get_at((shot_x, shot_y))
+        if colour == (0, 0, 0):
+            # We hit the ground
+            shot_in_flight = False
 
 
 def draw():
